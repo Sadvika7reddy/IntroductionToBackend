d5585e23cb94182e58d85d06e335619fa0716355
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-07 17:30:49 +0530

    http server

--- first-app.js
+++ first-app.js
@@ -13,14 +13,41 @@
 //     }
 // }
 // student.eligible()
-let fruits=["apple","oranges","","mango","","lemon"]
-let result=fruits.map((fruit)=>{
-    if(fruit==""){
-        fruit="empty string"
-    }
-    else{
-        fruit=fruit
-    }
-    return fruit
+// let fruits=["apple","oranges","","mango","","lemon"]
+// let result=fruits.map((fruit)=>{
+//     if(fruit==""){
+//         fruit="empty string"
+//     }
+//     else{
+//         fruit=fruit
+//     }
+//     return fruit
+// })
+// console.log(result)
+// async function Result(){
+//     console.log('a');
+
+//     console.log('b');
+//     let msg1=await new Promise((resolve,reject)=>{
+//         setTimeout(() => resolve('c'), 3000)
+    
+//     })
+//     console.log(msg1)
+//     let msg2=await new Promise((resolve,reject)=>{
+//         setTimeout(() => resolve('d'), 0)
+    
+//     })
+    
+//     console.log(msg2)
+//     console.log('e');  
+// }
+// Result();
+const http=require('http')
+let name="sadvika"
+const result=http.createServer((req,res)=>{
+   console.log(name)
 })
-console.log(result)
+result.listen(4000)
+
+
+
