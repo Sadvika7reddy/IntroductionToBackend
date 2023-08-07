adaf3c610bd7d3ff3ea59ebc770002e4ce221ba7
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-07 13:21:20 +0530

    product two numbers with arrow function

--- first-app.js
+++ first-app.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+// console.log("Hello World")
+let Product=(num1,num2)=>{
+    return num1*num2
+}
+
+console.log(Product(4,5))
+const student={
+    name:"sadvika",
+    age:23,
+    marks:53,
+    eligible(){
+        console.log(this.marks)
+    }
+}
+student.eligible()
