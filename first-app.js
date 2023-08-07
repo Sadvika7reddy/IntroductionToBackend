c213f183398e8f9bcf2dbcbef67ec307cf1e4af7
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-07 13:59:44 +0530

    used map

--- first-app.js
+++ first-app.js
@@ -1,15 +1,26 @@
 // console.log("Hello World")
-let Product=(num1,num2)=>{
-    return num1*num2
-}
+// let Product=(num1,num2)=>{
+//     return num1*num2
+// }
 
-console.log(Product(4,5))
-const student={
-    name:"sadvika",
-    age:23,
-    marks:53,
-    eligible(){
-        console.log(this.marks)
+// console.log(Product(4,5))
+// const student={
+//     name:"sadvika",
+//     age:23,
+//     marks:53,
+//     eligible(){
+//         console.log(this.marks)
+//     }
+// }
+// student.eligible()
+let fruits=["apple","oranges","","mango","","lemon"]
+let result=fruits.map((fruit)=>{
+    if(fruit==""){
+        fruit="empty string"
     }
-}
-student.eligible()
+    else{
+        fruit=fruit
+    }
+    return fruit
+})
+console.log(result)
