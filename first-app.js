00939589da55f6749aa10d0f4cd2cb09dadc572c
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-09 13:13:52 +0530

    added routing

--- first-app.js
+++ first-app.js
@@ -1,18 +1,21 @@
 
-const http=require('http')
+const bodyParser=require('body-parser')
 
 const express=require('express')
 
 const app=express()
 
-app.use((req,res,next)=>{
-    console.log("In the middleware")
-    next();
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"/><button type="submit">send</button></form>')
+    // console.log
 })
 
-app.use((req,res,next)=>{
-    console.log("In another middleware")
-    res.send('<h1>Hello from Express</h1>')
+app.use('/product',(req,res,next)=>{
+    
+     console.log(req.body)
+     res.redirect('/')   
 })
 
 
