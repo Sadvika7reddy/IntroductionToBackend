26440e51dcac47ccc71a5d3ff2dac09d229ef6ec
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-09 11:13:25 +0530

    implemented two middlewaare

--- first-app.js
+++ first-app.js
@@ -1,7 +1,22 @@
 
 const http=require('http')
-const router=require('./router')
-const result=http.createServer(router)
+
+const express=require('express')
+
+const app=express()
+
+app.use((req,res,next)=>{
+    console.log("In the middleware")
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("In another middleware")
+    res.send('<h1>Hello from Express</h1>')
+})
+
+
+
    
-result.listen(8000)
+app.listen(4000)
 
