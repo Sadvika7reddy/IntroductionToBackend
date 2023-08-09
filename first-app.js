9f4cc9790bd45a5bf34ae91772a2d3a501201656
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-09 14:51:22 +0530

    routes and filter

--- first-app.js
+++ first-app.js
@@ -3,22 +3,20 @@ const bodyParser=require('body-parser')
 
 const express=require('express')
 
+const adminRouter=require('./routes/admin')
+const shopRouter=require('./routes/shop')
+
 const app=express()
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"/><button type="submit">send</button></form>')
-    // console.log
+
+app.use('/admin',adminRouter)
+app.use('/shop',shopRouter)
+
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>Page not found</h1>")
 })
-
-app.use('/product',(req,res,next)=>{
-    
-     console.log(req.body)
-     res.redirect('/')   
-})
-
-
 
    
 app.listen(4000)
